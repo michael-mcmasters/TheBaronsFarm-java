d80bff888753178fbce0319ff346ab67280691a9
michael-mcmasters <jmcmasters411@gmail.com>
2020-12-11 13:10:28 -0500

    Complete Farmer. Have it instantiate Farm in constructor

--- src/main/java/hbcu/stay/ready/baronsfarm/Farmer.java
+++ src/main/java/hbcu/stay/ready/baronsfarm/Farmer.java
@@ -3,6 +3,13 @@ package hbcu.stay.ready.baronsfarm;
 import hbcu.stay.ready.baronsfarm.interfaces.*;
 
 public class Farmer extends Person implements Rider, Botanist {
+
+    private Farm farm;
+
+    public Farmer() {
+        this.farm = new Farm();
+    }
+
     public void mouseRideable(Rideable rideable) {
 
     }
