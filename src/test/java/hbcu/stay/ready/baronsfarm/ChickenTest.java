95b9e5ac627ace8da96cce5959b4bb98ef6a08b7
michael-mcmasters <jmcmasters411@gmail.com>
2020-12-11 18:40:58 -0500

    Add @Before annotation

--- src/test/java/hbcu/stay/ready/baronsfarm/ChickenTest.java
+++ src/test/java/hbcu/stay/ready/baronsfarm/ChickenTest.java
@@ -1,20 +1,31 @@
 package hbcu.stay.ready.baronsfarm;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ChickenTest {
 
+    private Chicken chicken;
+
+    @Before
+    public void init() {
+        chicken = new Chicken();
+    }
+
     @Test
     public void makeNoiseTest() {
-        // Given
-        Chicken chicken = new Chicken();
-
-        // When
-        String expected = "bwuak";
         String actual = chicken.makeNoise();
+        Assert.assertEquals("bwuak", actual);
+    }
 
-        // Then
-        Assert.assertEquals(expected, actual);
+    @Test
+    public void harvestTest() {
+        // ToDo
+    }
+
+    @Test
+    public void fertilizeTest() {
+        // ToDo
     }
 }
