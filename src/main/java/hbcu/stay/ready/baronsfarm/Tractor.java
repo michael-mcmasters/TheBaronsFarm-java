ccc2b0523ded66f91e085d9ef0b870d961a3c970
michael-mcmasters <jmcmasters411@gmail.com>
2020-12-11 21:43:45 -0500

    Complete Tractor and TractorTest

--- src/main/java/hbcu/stay/ready/baronsfarm/Tractor.java
+++ src/main/java/hbcu/stay/ready/baronsfarm/Tractor.java
@@ -1,14 +1,32 @@
 package hbcu.stay.ready.baronsfarm;
 
+import hbcu.stay.ready.baronsfarm.interfaces.Crop;
 import hbcu.stay.ready.baronsfarm.interfaces.FarmVehicle;
+
+import java.util.ArrayList;
 
 public class Tractor implements FarmVehicle {
 
+    // Operate over entire farm.
     public void operate(Farm farm) {
+        ArrayList<Field> fields = farm.getFieldList();
+        for (Field field : fields) {
+            harvest(field);
+        }
+    }
 
+    // Field holds CropRow(s). Fertilize Crop(s) in CropRow(s).
+    private void harvest(Field field) {
+        ArrayList<CropRow> cropRows = field.getCropRows();
+        for (CropRow cropRow : cropRows) {
+            ArrayList<Crop> crops = cropRow.getCrops();
+            for (Crop crop : crops) {
+                crop.harvest();
+            }
+        }
     }
 
     public String makeNoise() {
-        return null;
+        return "bruhbruhbruhburh";
     }
 }
---
+++ src/test/java/hbcu/stay/ready/baronsfarm/TractorTest.java
@@ -0,0 +1,33 @@
+package hbcu.stay.ready.baronsfarm;
+
+import hbcu.stay.ready.baronsfarm.interfaces.Crop;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TractorTest {
+
+    Tractor tractor;
+
+    @Before
+    public void init() {
+        tractor = new Tractor();
+    }
+
+    @Test
+    public void operateTest() {
+        Crop cornStalk = new CornStalk();
+        CropRow cropRow = new CropRow(cornStalk);
+        Field field = new Field(cropRow);
+        Farm farm = new Farm();
+        farm.addToFieldList(field);
+
+        tractor.operate(farm);
+        Assert.assertTrue(cornStalk.getHasBeenHarvested());
+    }
+
+    @Test
+    public void makeNoiseTest() {
+        Assert.assertEquals("bruhbruhbruhburh", tractor.makeNoise());
+    }
+}
